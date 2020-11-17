a7c11bdce3681128b3ed562c5c106d15f141fe1e
alvaro <almamani@agetic.gob.bo>
2020-11-16 23:46:45 -0400

    crear base de servidor

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const SocketIo = require('socket.io');
+
+
+// settings
+app.set('port', process.env.PORT || 3000);
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+const server = app.listen(app.get('port'), () => {
+  console.log(`Server listening on ${app.get('port')}`);
+});
+
+const io = SocketIo.listen(server);
+
+// websockets
+io.on('connection', (socket) => {
+  console.log('new connection', socket.id);
+});
